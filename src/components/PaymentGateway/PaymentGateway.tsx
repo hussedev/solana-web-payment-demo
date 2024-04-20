3f90c668958e662b6521b1e0d703cba7dfec312a
Hussein Martinez <husse.dev@gmail.com>
2024-04-20 16:52:41 +0700

    feat: implemented minimal PaymentGateway

---
+++ src/components/PaymentGateway/PaymentGateway.tsx
@@ -0,0 +1,75 @@
+import { FC, useCallback } from "react";
+import { WalletNotConnectedError } from "@solana/wallet-adapter-base";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import {
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+
+const testPaymentPubKey = new PublicKey(
+  "Ap6RWaUeZWefQ6i2jFSb4tn9RhnbeVfQwJfsREh5HSRa"
+);
+
+export const PaymentGateway: FC = () => {
+  const { connection } = useConnection();
+
+  const { wallet, publicKey, sendTransaction } = useWallet();
+
+  const onClick = useCallback(async () => {
+    if (!publicKey) throw new WalletNotConnectedError();
+
+    // 890880 lamports as of 2022-09-01
+    const lamports = await connection.getMinimumBalanceForRentExemption(0);
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey: testPaymentPubKey,
+        lamports,
+      })
+    );
+
+    const {
+      context: { slot: minContextSlot },
+      value: { blockhash, lastValidBlockHeight },
+    } = await connection.getLatestBlockhashAndContext();
+
+    const signature = await sendTransaction(transaction, connection, {
+      minContextSlot,
+    });
+
+    await connection.confirmTransaction({
+      blockhash,
+      lastValidBlockHeight,
+      signature,
+    });
+  }, [publicKey, sendTransaction, connection]);
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            margin: "10px",
+          }}
+        >
+          {wallet ? <WalletDisconnectButton /> : <WalletMultiButton />}
+        </div>
+        <div>
+          <button onClick={onClick} disabled={!publicKey}>
+            Make Test Payment!
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
